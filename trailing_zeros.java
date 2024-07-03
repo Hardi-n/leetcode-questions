ccdcb0c739b29e2ee74d29f8ffb82732f1867e17
Hardi-n <subhagyachhabra18718@gmail.com>
2024-07-03 12:12:55 +0530

    changed the approach of the trailing zeros because it is not applicabe for the lager value of given integer n

--- trailing_zeros.java
+++ trailing_zeros.java
@@ -35,29 +35,29 @@ Constraints:
 
 
 
-class Solution {
-    public int trailingZeroes(int n) {
-        long result = factorialIterative(n);
-        int returningvariable = countTrailingZeros(result);
-        return returningvariable;
-    }
-    public static long factorialIterative(int n) {
-        long result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        return result;
-    }
-    public static int countTrailingZeros(long n) {
-        int count = 0;
-        // Continue dividing by 10 until the number is no longer divisible by 10
-        while (n % 10 == 0 && n != 0) {
-            count++;
-            n /= 10;
-        }
-        return count;
-    }
-}
+// class Solution {
+//     public int trailingZeroes(int n) {
+//         long result = factorialIterative(n);
+//         int returningvariable = countTrailingZeros(result);
+//         return returningvariable;
+//     }
+//     public static long factorialIterative(int n) {
+//         long result = 1;
+//         for (int i = 1; i <= n; i++) {
+//             result *= i;
+//         }
+//         return result;
+//     }
+//     public static int countTrailingZeros(long n) {
+//         int count = 0;
+//         // Continue dividing by 10 until the number is no longer divisible by 10
+//         while (n % 10 == 0 && n != 0) {
+//             count++;
+//             n /= 10;
+//         }
+//         return count;
+//     }
+// }
 
 
 
@@ -67,4 +67,37 @@ class Solution {
  * 1. firstly calculate the factorial of a number
  * 2. calculate trailing zeros by checking how many times a number can be divided by 10
  * 3. return the result
+ */
+
+
+
+
+ class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        // Count factors of 5 in all numbers from 1 to n
+        while (n > 0) {
+            n /= 5;
+            count += n;
+        }
+        return count;
+    }
+}
+
+
+/*
+ * 
+ * changed the approach because it is not applicable for the larger values of the given n
+ * 
+ * Explanation:
+
+1.Initialization: 
+    count is initialized to 0 to keep track of the number of trailing zeros.
+
+2.Counting Factors of 5:
+    In each iteration of the while loop, n is divided by 5.
+    The integer result of this division is added to count.
+    This process continues until n is reduced to 0.
+    
+3.Return: The final value of count is the number of trailing zeros in n!.
  */
