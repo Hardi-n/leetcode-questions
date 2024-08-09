50df511ac8904d818f4de95fad1ccda9bbcc9438
Hardi-n <subhagyachhabra18718@gmail.com>
2024-08-09 17:41:57 +0530

    updated third maximum using 3 variable

--- third_maximum_number.java
+++ third_maximum_number.java
@@ -71,4 +71,41 @@ Convert to Array and Sort:
 Convert the HashSet to an array and sort it in descending order.
 Return the Fourth Maximum:
 Check if the array length is at least 4. If so, return the fourth element (index 3). Otherwise, return the maximum number (index 0).
- */
+ */\
+
+
+
+
+//  corrected code ---->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+class Solution {
+    public int thirdMax(int[] nums) {
+       Integer max1 = null;
+        Integer max2 = null;
+        Integer max3 = null;
+        
+        for (Integer num : nums) {
+            // Skip the number if it's already considered as one of the max values
+            if (num.equals(max1) || num.equals(max2) || num.equals(max3)) {
+                continue;
+            }
+            
+            if (max1 == null || num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (max2 == null || num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (max3 == null || num > max3) {
+                max3 = num;
+            }
+        }
+        
+        // Return max3 if it's set; otherwise, return max1
+        return max3 == null ? max1 : max3;
+    }
+}
